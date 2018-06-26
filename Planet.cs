cb27be893af73174509872b865a2034448090380
Riley Strickland <Github@rileystrickland.com>
2018-06-26 03:20:42 -0400

    Planet ✔️
    This class is feature ready.

--- Planet.cs
+++ Planet.cs
@@ -32,7 +32,7 @@ namespace Saliens
     }
     public class Planet
     {
-        #region static fields
+        #region Static Fields
         private static PlanetEqualityComparer PlanetEqualityComparer = new PlanetEqualityComparer { };
         private static ConcurrentDictionary<int, Planet> _cache = new ConcurrentDictionary<int, Planet>();
         #endregion
@@ -54,86 +54,42 @@ namespace Saliens
         public Zone[] Zones { get; private set; }
         #endregion
 
-        /// <summary>
-        /// Gets the first zone it can find.  Sorted by difficulty, then by capture progress.
-        /// </summary>
-        public Zone FirstAvailableZone => Zones.Where(x => !x.Captured).OrderByDescending(x => x.Difficulty).ThenBy(x => x.CaptureProgress).First();
-
+        #region Instance Properties
+        [JsonIgnore]
+        public IEnumerable<Zone> ActiveZones => Zones.Where(z => !z.Captured);
+        [JsonIgnore]
+        public IEnumerable<Zone> CapturedZones => Zones.Where(z => z.Captured);
         /// <summary>
         /// Gets only zones that match the difficulty.
         /// </summary>
         /// <param name="difficulty">Difficulty of the zones to find.  (Easy, Medium, Hard)</param>
         /// <returns>The zones that matched the filter.</returns>
         public Zone[] FilterAvailableZones(ZoneDifficulty difficulty) => Zones.Where(x => x.Captured == false && x.Difficulty == difficulty).ToArray();
-
-
+        /// <summary>
+        /// Gets the first zone it can find.  Sorted by difficulty, then by capture progress.
+        /// </summary>
+        public Zone FirstAvailableZone => Zones.Where(x => !x.Captured).OrderByDescending(x => x.Difficulty).ThenBy(x => x.CaptureProgress).First();
         /// <summary>
         /// Gets the zone that matches zoneID.
         /// </summary>
         /// <param name="zoneID">ID of the zone.</param>
         /// <returns></returns>
-        public Zone GetZoneFromID(int zoneID) => Zones.Where(x => x.GameID == zoneID).FirstOrDefault();
-
+        public Zone GetZoneFromID(int zoneID) => Zones.Where(x => x.GameID == zoneID).FirstOrDefault();       
         /// <summary>
         /// Gets the zone that matches zonePosition.
         /// </summary>
         /// <param name="zonePosition">Position of the zone.</param>
         /// <returns></returns>
         public Zone GetZoneFromPosition(int zonePosition) => Zones.Where(x => x.Position == zonePosition).FirstOrDefault();
+        #endregion
 
-
-        public static Planet Update(int ID)
-        {
-            string JSON = Network.Get("GetPlanet", Network.EndPoint.ITerritoryControlMinigameService, "id", ID.ToString(), "language", "english").GetAwaiter().GetResult();
-            Planet planet = Network.Deserialize<PlanetResponse>(JSON).Planets[0];
-            return _cache.AddOrUpdate(ID, planet, (key, oldvalue) => oldvalue = planet);
-        }
-        public static Planet Update(Planet planet) => Update(planet.ID);
-        public static Planet Get(int ID) => _cache.GetOrAdd(ID, Update(ID));
-        public static IReadOnlyList<Planet> UpdateActive() => MultiUpdate(true);
-        public static IReadOnlyList<Planet> UpdateAll() => MultiUpdate(false); //Usually you only need to call this ONE time.
-
-
-        /// <summary>
-        /// Gets the planets in the game.
-        /// </summary>
-        /// <param name="ActiveOnly">Only get the planets that are currently active.</param>
-        /// <returns>Planets that match the filter.</returns>
-        private static IReadOnlyList<Planet> MultiUpdate(bool ActiveOnly = true)
-        {
-            string JSON = Network.Get("GetPlanets", Network.EndPoint.ITerritoryControlMinigameService, "active_only", ActiveOnly.AsInt(), "language", "english").GetAwaiter().GetResult();
-            List<Planet> planets = Network.Deserialize<PlanetResponse>(JSON).Planets;
-
-            if (ActiveOnly) planets.AddRange(Active.Except(planets, PlanetEqualityComparer));
-
-            foreach (Planet planet in planets)
-            {
-                Update(planet);
-            }
-
-            return planets;
-        }
-
+        #region Static Properties
         [JsonIgnore]
         public static IEnumerable<Planet> All => _cache.Values;
-
         [JsonIgnore]
         public static IEnumerable<Planet> Active => _cache.Values.Where(x => x.Info.Active && !x.Info.Captured);
-
         [JsonIgnore]
         public static IEnumerable<Planet> Captured => _cache.Values.Where(x => x.Info.Captured);
-
-        [JsonIgnore]
-        public static IEnumerable<Planet> Locked => _cache.Values.Where(x => !x.Info.Active && !x.Info.Captured);
-
-
-
-        [JsonIgnore]
-        public IEnumerable<Zone> ActiveZones => Zones.Where(z => !z.Captured);
-
-        [JsonIgnore]
-        public IEnumerable<Zone> CapturedZones => Zones.Where(z => z.Captured);
-
         /// <summary>
         /// Finds the first planet with the hardest difficulty zone, then least captured.
         /// </summary>
@@ -155,6 +111,34 @@ namespace Saliens
                 throw new NoPlanetException();
             }
         }
+        [JsonIgnore]
+        public static IEnumerable<Planet> Locked => _cache.Values.Where(x => !x.Info.Active && !x.Info.Captured);
+        #endregion
+
+        #region Static Methods
+        public static async Task<Planet> Get(int ID) => _cache.GetOrAdd(ID, await Update(ID));
+        /// <summary>
+        /// Gets the planets in the game.
+        /// </summary>
+        /// <param name="ActiveOnly">Only get the planets that are currently active.</param>
+        /// <returns>Planets that match the filter.</returns>
+        private static async Task<IReadOnlyList<Planet>> MultiUpdate(bool ActiveOnly = true)
+        {
+            string JSON = await Network.Get("GetPlanets", Network.EndPoint.ITerritoryControlMinigameService, "active_only", ActiveOnly.AsInt(), "language", "english");
+            List<Planet> planets = Network.Deserialize<PlanetResponse>(JSON).Planets;
+            if (ActiveOnly) planets.AddRange(Active.Except(planets, PlanetEqualityComparer));
+            return await Task.WhenAll(planets.Select(x => Update(x)));
+        }
+        public static async Task<Planet> Update(int ID)
+        {
+            string JSON = await Network.Get("GetPlanet", Network.EndPoint.ITerritoryControlMinigameService, "id", ID.ToString(), "language", "english");
+            Planet planet = Network.Deserialize<PlanetResponse>(JSON).Planets[0];
+            return _cache.AddOrUpdate(ID, planet, (key, oldvalue) => oldvalue = planet);
+        }
+        public static async Task<Planet> Update(Planet planet) => await Update(planet.ID);
+        public static async Task<IReadOnlyList<Planet>> UpdateActive() => await MultiUpdate(true);
+        public static async Task<IReadOnlyList<Planet>> UpdateAll() => await MultiUpdate(false); //Usually you only need to call this ONE time.
+        #endregion
     }
 
     public static class IEnumerablePlanetExtensions
